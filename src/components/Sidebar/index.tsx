51cdceda3e09ac2d15903529e008f1d469e47638
narmingeybullayeva <narmin.g.mammadli@gmail.com>
2025-08-06 15:36:53 +0400

    Implement code changes to enhance functionality and improve performance

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FileText, Users, Route } from 'lucide-react';
+import Img from '../../assets/images/caspian.png';
 
 interface SidebarProps {
     isOpen: boolean;
@@ -16,24 +17,21 @@ const menuItems = [
 const Sidebar: React.FC<SidebarProps> = ({ isOpen, activeItem, onItemClick }) => {
     return (
         <div className={`
-      fixed left-0 top-0 h-full bg-gray-50 dark:bg-gray-800 transition-all duration-300 z-30
+      fixed top-0 left-0 h-full bg-gray-50 dark:bg-gray-800 transition-all duration-300 
       ${isOpen ? 'w-64' : 'w-0 md:w-64'}
       overflow-hidden
     `}>
-            <div className="p-6">
-                <div className="mb-8">
-                    <div className="flex items-center gap-3">
-                        <div className="w-10 h-10 bg-gradient-to-br from-blue-500 to-green-500 rounded-lg flex items-center justify-center">
-                            <span className="text-white font-bold text-lg">A</span>
-                        </div>
-                        <div>
-                            <h1 className="text-sm font-semibold text-gray-900 dark:text-white">ASPLANNet</h1>
-                            <p className="text-xs text-gray-600 dark:text-gray-400">İqtisadi təhlil</p>
-                        </div>
+            <div >
+                <div className="mb-3">
+                    <div className="flex mx-auto items-center justify-left">
+
+                        <img src={Img} alt="Logo" className="h-20 p-4" />
+
+
                     </div>
                 </div>
 
-                <nav className="space-y-2">
+                <nav className="space-y-2 px-4">
                     <div className="text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider mb-4">
                         MENU
                     </div>
@@ -46,8 +44,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, activeItem, onItemClick }) =>
                                 className={`
                   w-full flex items-center gap-3 px-4 py-3 rounded-lg text-left transition-all duration-200
                   ${activeItem === item.id
-                                        ? 'bg-blue-500 text-white shadow-md'
-                                        : 'text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-700'
+                                        ? 'bg-gray-100 text-blue-500 shadow-md'
+                                        : 'text-gray-700 dark:text-gray-300 hover:bg-gray-100  dark:hover:bg-gray-700'
                                     }
                 `}
                             >
